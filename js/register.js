0a159114eeaaecaac7dae644ced433bc670f100e
Emmanuel Victor <emavec2002@gmail.com>
2025-04-01 17:33:57 -0700

    (;

--- js/register.js
+++ js/register.js
@@ -128,9 +128,42 @@ const firebaseConfig = {
                 })
                 
                 isformData = true
+            } else {
+              
+                const Toast = Swal.mixin({
+                  toast: true,
+                  position: "top-end",
+                  showConfirmButton: false,
+                  timer: 3000,
+                  timerProgressBar: true,
+                  didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                  }
+                });
+                Toast.fire({
+                  icon: "warning",
+                  title: "Password: Min of 8 characters"
+                });
+
             }
         } else {
 
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+              }
+            });
+            Toast.fire({
+              icon: "warning",
+              title: "Fill in all inputs"
+            });
         }
         
         console.log(formData, isformData)
